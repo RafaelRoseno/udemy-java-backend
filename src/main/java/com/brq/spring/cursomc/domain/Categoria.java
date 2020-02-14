281a8fb2fb1ca7ededf11a330a48d070234f618c
Rafael Roseno <rafaelroseno@outlook.com>
2020-02-14 11:25:04 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/brq/spring/cursomc/domain/Categoria.java
+++ src/main/java/com/brq/spring/cursomc/domain/Categoria.java
@@ -1,24 +1,26 @@
 package com.brq.spring.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -45,6 +47,14 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -67,6 +77,8 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
+
+	
 	
 	
 	
